0913e10616991e29defd569de10cc249b49b39c7
Paul D Handy <paul.d.handy@gmail.com>
2017-05-04 14:22:00 -0600

    removed nextChannelKey

--- test/mam/mam.newMaskedAuthenticationMessage.js
+++ test/mam/mam.newMaskedAuthenticationMessage.js
@@ -12,7 +12,6 @@ describe('MerkleTree.get', function() {
         {
             seed: "KXRVLFETGUTUWBCNCC9DWO99JQTEI9YXVOZHWELSYP9SG9KN9WCKXOVTEFHFH9EFZJKFYCZKQPPBXYSGJ",
             encryptionSeed: "9XRVLFETGUTUWBCNCC9DWO99JQTEI9YXVOZHWELSYP9SG9KN9WCKXOVTEFHFH9EFZJKFYCZKQPPBXYSGJ",
-            nextEncSeed: "AYZVLFETGUTUWBCNCC9DWO99JQTEI9YXVOZHWELSYP9SG9KN9WCKXOVTEFHFH9EFZJKFYCZKQPPBXYSGJ",
             message: "HELLOWORLDIAMATRYTE9ENCODEDSTRINGMADEBYIOTALIB9JSUTILASCIITOTRYTES",
             start: 3,
             count: 9,
@@ -32,7 +31,6 @@ describe('MerkleTree.get', function() {
                     merkleTree: tree0, 
                     nextRoot: tree1.root.hash.toString(), 
                     channelKey: test.encryptionSeed, 
-                    nextChannelKey: test.nextEncSeed, 
                     index: index
                 });
                 var length = mamTransactions[0].message.length;
