8d1c0cee2a43c7fceabc43d310f9d0ec6ae9bd12
Paul D Handy <paul.d.handy@gmail.com>
2017-05-07 22:13:59 -0600

    Corrected channel key generation to spec

--- examples/getMessage.js
+++ examples/getMessage.js
@@ -1,30 +1,29 @@
 const IOTA = require('iota.lib.js');
 const MAM = require('../lib/mam');
 const MerkleTree = require('../lib/merkle');
+const Encryption = require('../lib/encryption');
+var Crypto = require('crypto.iota.js');
 
 const iota = new IOTA({
   provider: 'http://localhost:14700'
 });
 
 const seed = 'PAULUNOZTUVHPBKLTFVRJZTOPODGTYHRUIACDYDKRNAQMCUZGNWMDSDZMPWHKQINYFPYTIEDSZ9EJZYOD';
-const message = "moshi moshi";
-const encryptionKeyIndex = 1;
-const channelKeyIndex = 1;
+const channelKeyIndex = 3;
+const channelKey = Crypto.converter.trytes(Encryption.subseed(Encryption.hash(Encryption.increment(Crypto.converter.trits(seed.slice()))), channelKeyIndex));
 const start = 3;
 const count = 4;
 const security = 1;
 
 const tree0 = new MerkleTree(seed, start, count, security);
-const tree1 = new MerkleTree(seed, start + count, count, security);
-let index = 1;
 
 const root = tree0.root.hash.toString();
 iota.api.sendCommand({ 
     command: "MAM.getMessage",
-    channel: MAM.messageID(seed)
+    channel: MAM.messageID(channelKey)
 }, function(e, result) {
     if(e == undefined) {
-        const output = MAM.parse(result.ixi, {seed: seed});
+        const output = MAM.parse(result.ixi, {key: channelKey});
         const asciiMessage = iota.utils.fromTrytes(output.message);
         if (root === output.root) {
             console.log("Public key match for " + root); 
--- examples/post.js
+++ examples/post.js
@@ -2,36 +2,32 @@ const IOTA = require('iota.lib.js');
 const MAM = require('../lib/mam');
 const MerkleTree = require('../lib/merkle');
 const Encryption = require('../lib/encryption');
+var Crypto = require('crypto.iota.js');
 
 const iota = new IOTA({
   provider: 'http://localhost:14700'
 });
 
 const seed = 'PAULUNOZTUVHPBKLTFVRJZTOPODGTYHRUIACDYDKRNAQMCUZGNWMDSDZMPWHKQINYFPYTIEDSZ9EJZYOD';
-/*
-const seed = 'UYRCUNOZTUVHPBKLTFVRJZTOPODGTYHRUIACDYDKRNAQMCUZGNWMDSDZMPWHKQINYFPYTIEDSZ9EJZYOD';
-const encryptionSeed = 'JLHQLDCYRVNJVOOUQ9DDZXJYOQVODPXS9XPXDLTLAOIAYANJZOM9KHLFUVLARJKLBVH9GPYFKKACWRUQL';
-*/
-const message = "moshi moshi";
-const encryptionKeyIndex = 1;
-const channelKeyIndex = 1;
+//const message = "\"'I'm here for you in the same way that you're here for me, each person is an intricate piece of infinity. -Eyedea\" - Dukakis";
+const message = "\"'I'm still here for IOTA in the same way that you're here for me, each person is an intricate piece of infinity. -Eyedea\" - Dukakis";
+const channelKeyIndex = 3;
+const channelKey = Crypto.converter.trytes(Encryption.subseed(Encryption.hash(Encryption.increment(Crypto.converter.trits(seed.slice()))), channelKeyIndex));
 const start = 3;
 const count = 4;
 const security = 1;
 
 const tree0 = new MerkleTree(seed, start, count, security);
 const tree1 = new MerkleTree(seed, start + count, count, security);
-let index = 1;
+let index = 0;
 
 // Get the trytes of the MAM transactions
 const trytes = new MAM.MaskedAuthenticatedMessage({
     message: iota.utils.toTrytes(message),
     merkleTree: tree0,
+    index: index,
     nextRoot: tree1.root.hash.toString(),
-    channelKey: seed,
-    channelKeyIndex: channelKeyIndex,
-    encryptionKeyIndex: encryptionKeyIndex,
-    index: index
+    channelKey: channelKey,
 });
 
 // Depth
